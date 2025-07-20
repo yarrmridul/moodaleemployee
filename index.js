1602b04ce1b0cf3d8ef112e2185392ba611e739a
Mridul Agrawal <mridulagrawal06@gmail.com>
2025-07-21 01:27:18 +0530

    Fixed env config n

--- index.js
+++ index.js
@@ -102,10 +102,11 @@ const path = require("path");
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "moodale",
-  password: "Abnhibhulunga@07",
+    host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
 });
 console.log("Starting server...");
 
