ff2c5bed7ca77d01d43509f7ba78464a55b450e8
Mridul Agrawal <mridulagrawal06@gmail.com>
2025-07-21 01:31:18 +0530

    Fixed env confign

--- index.js
+++ index.js
@@ -106,6 +106,8 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+
 
 });
 console.log("Starting server...");
