113cc5b5f6d27c58a51e417e8413082d95b1b64a
Mridul Agrawal <mridulagrawal06@gmail.com>
2025-07-21 11:38:43 +0530

    sql remote db added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-
+//https://freedb.tech/dashboard/
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
